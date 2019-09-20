6cd745d5a4dbbf4b345166f83b76f439ac5ca9da
Miriam Corona <mcorona@nearsoft.com>
2019-09-19 18:11:42 -0700

    added string elements

--- src/test/java/pageobjects/CreateAccountPage.java
+++ src/test/java/pageobjects/CreateAccountPage.java
@@ -2,5 +2,21 @@ package pageobjects;
 
 public class CreateAccountPage {
 
+    String firstNameAddress = "#firstname.form-control";
+    String lastNameAddress = "#lastname.form-control";
+    String companyNameAddress = "#company.form-control";
+    String address1Address = "#address1.form-control";
+    String address2Address = "#address2.form-control";
+    String cityAddress = "#city.form-control";
+    String stateDropdownAddress = "#id_state.form-control";
+    String zipCodeAddress = "#postcode.form-control";
+    String countryDropdownAddress = "#id_country.form-control";
+    String additionalInfoAddress = "#other.form-control";
+    String homePhoneAddress = "#phone.form-control";
+    String mobilePhoneAddress = "#phone_mobile.form-control";
+    String addressAliasAddress = "#alias.form-control";
+    String registerButton = "#submitAccount";
+
+
 
 }
