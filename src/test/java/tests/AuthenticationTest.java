066475558d94f86ac66425e586e388a6aeefbade
Miriam Corona <mcorona@nearsoft.com>
2019-09-24 17:46:04 -0700

    added list of test scenarios

---
+++ src/test/java/tests/AuthenticationTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class AuthenticationTest extends BaseTest {
+
+    @Test
+    public void createAccountWithEmptyEmail(){
+    }
+
+    @Test
+    public void createAccountWithInvalidEmail(){
+    }
+
+    @Test
+    public void createAccountWithRegisteredEmail(){
+
+    }
+
+    @Test
+    public void createAccountWithalValidEmail(){}
+
+}
