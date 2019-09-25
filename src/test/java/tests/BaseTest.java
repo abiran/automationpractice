6d7fb1289687d509623fcfe8a48ce89d068a9112
Alejandro Ramirez <cloud91art@gmail.com>
2019-09-24 18:16:01 -0700

    Added generic wait

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,11 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -11,12 +14,13 @@ public class BaseTest {
     private WebDriver driver;
 
     @BeforeTest
-    public void setUp() {
+    public void setUp() throws InterruptedException {
 //        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/vendor/chromedriver");
 //        vendor no longer needed
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("http://automationpractice.com/index.php");
+        wait3Secs();
     }
 
     @AfterTest
@@ -26,6 +30,10 @@ public class BaseTest {
 
     public WebDriver getWebDriver(){
         return driver;
+    }
+
+    public void wait3Secs() throws InterruptedException {
+        Thread.sleep(3000);
     }
 
 
