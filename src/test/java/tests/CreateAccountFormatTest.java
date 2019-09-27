21e0c6f17f4a81ec505139e4156390996b00a5c6
rtopete <rtopete@switchfly.com>
2019-09-26 18:49:25 -0700

    Updated CreateAccountFormatTest Adding CreateAccountSuccessfullyCreated class

--- src/test/java/tests/CreateAccountFormatTest.java
+++ src/test/java/tests/CreateAccountFormatTest.java
@@ -76,43 +76,4 @@ public class CreateAccountFormatTest extends BaseTest {
 
         Assert.assertTrue(createAccountPage.isAlertDisplayed());
     }
-
-    //rtr
-    @Test
-    public void createAccountSuccessfully() throws InterruptedException {
-        HomePage homePage = new HomePage(getWebDriver());
-        homePage.SignInClick();
-        wait3Secs();
-        SignInPage signInPage = new SignInPage(getWebDriver());
-
-        signInPage.sendKeysCreateAccountEmailAddress(getRandomEmail());
-        signInPage.clickCreateAccountButton();
-        wait3Secs();
-
-        CreateAccountPage createAccountPage = new CreateAccountPage(getWebDriver());
-        //wait3Secs();
-        createAccountPage.selectGenderMale();
-        createAccountPage.typeFirstNameCustomer("Rodrigo");
-        createAccountPage.typeLastNameCustomer("Topete");
-        createAccountPage.typePassword("Password1");
-        createAccountPage.setDOB("10","October","1980");
-        createAccountPage.typeFirstNameAddress("Rodrigo");
-        createAccountPage.typeLastNameAddress("Topete");
-        createAccountPage.typeAddress1Address("123 Street");
-        createAccountPage.typeCityAddress("City1");
-        createAccountPage.selectStateAddress("Arizona");
-        createAccountPage.typeZipCodeAddress("00000");
-        createAccountPage.selectCountryAddress("United States");
-        createAccountPage.typeMobilePhoneAddress("1212121212");
-        createAccountPage.clickRegisterButton();
-        wait3Secs();
-
-        MyAccountPage myAccountPage = new MyAccountPage(getWebDriver());
-
-        assertTrue(myAccountPage.getHeaderText().equals("MY ACCOUNT"));
-
-        System.out.println("rtr");
-
-
-    }
 }
---
+++ src/test/java/tests/CreateAccountSuccessfully.java
@@ -0,0 +1,51 @@
+package tests;
+
+import org.testng.annotations.Test;
+import pageobjects.CreateAccountPage;
+import pageobjects.HomePage;
+import pageobjects.MyAccountPage;
+import pageobjects.SignInPage;
+
+import static org.testng.Assert.assertTrue;
+
+public class CreateAccountSuccessfully extends BaseTest{
+
+
+    @Test
+    public void createAccountSuccessfully() throws InterruptedException {
+        HomePage homePage = new HomePage(getWebDriver());
+        homePage.SignInClick();
+        wait3Secs();
+        SignInPage signInPage = new SignInPage(getWebDriver());
+
+        signInPage.sendKeysCreateAccountEmailAddress(getRandomEmail());
+        signInPage.clickCreateAccountButton();
+        wait3Secs();
+
+        CreateAccountPage createAccountPage = new CreateAccountPage(getWebDriver());
+        //wait3Secs();
+        createAccountPage.selectGenderMale();
+        createAccountPage.typeFirstNameCustomer("Rodrigo");
+        createAccountPage.typeLastNameCustomer("Topete");
+        createAccountPage.typePassword("Password1");
+        createAccountPage.setDOB("10","October","1980");
+        createAccountPage.typeFirstNameAddress("Rodrigo");
+        createAccountPage.typeLastNameAddress("Topete");
+        createAccountPage.typeAddress1Address("123 Street");
+        createAccountPage.typeCityAddress("City1");
+        createAccountPage.selectStateAddress("Arizona");
+        createAccountPage.typeZipCodeAddress("00000");
+        createAccountPage.selectCountryAddress("United States");
+        createAccountPage.typeMobilePhoneAddress("1212121212");
+        createAccountPage.clickRegisterButton();
+        wait3Secs();
+
+        MyAccountPage myAccountPage = new MyAccountPage(getWebDriver());
+
+        assertTrue(myAccountPage.getHeaderText().equals("MY ACCOUNT"));
+
+        System.out.println("rtr");
+
+
+    }
+}
