1bafa908852c6cb0b135d8deb24aca221aca7618
alopez <alopez@switchfly.com>
2019-10-08 16:27:21 -0700

    Cositas

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,7 +1,12 @@
 package tests;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.SecureRandom;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -19,14 +24,17 @@ public class BaseTest {
 //        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/vendor/chromedriver");
 //        vendor no longer needed
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(); //you could send setupChromeOptions as param
+        driver = new ChromeDriver(setupChromeOptions()); //you could send setupChromeOptions as param
         driver.get("http://automationpractice.com/index.php");
         _pageFactory = new PageFactory( driver );
         wait3Secs();
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown() throws IOException {
+        String actualTest = this.getClass().getName();
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs( OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("target/screenshots/"+actualTest+".png"));
         driver.quit();
     }
 
@@ -62,4 +70,6 @@ public class BaseTest {
         }
         return sb.toString() + "@testing.com";
     }
+
+
 }
