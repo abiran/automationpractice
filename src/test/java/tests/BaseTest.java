69f4fe2b4a55413b0d173502c31deea9594949a5
alopez <alopez@switchfly.com>
2019-10-08 16:39:17 -0700

    Cositas

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,12 +1,8 @@
 package tests;
 
-import java.io.File;
 import java.io.IOException;
 import java.security.SecureRandom;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -32,9 +28,9 @@ public class BaseTest {
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() throws IOException {
-        String actualTest = this.getClass().getName();
-        File scrFile = ((TakesScreenshot)driver).getScreenshotAs( OutputType.FILE);
-        FileUtils.copyFile(scrFile, new File("target/screenshots/"+actualTest+".png"));
+//        String actualTest = this.getClass().getName();
+//        File scrFile = ((TakesScreenshot)driver).getScreenshotAs( OutputType.FILE);
+//        FileUtils.copyFile(scrFile, new File("target/screenshots/"+actualTest+".png"));
         driver.quit();
     }
 
