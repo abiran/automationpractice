8a542cd1427cf4bb3ea230ffbfab4e36b8e99944
alopez <alopez@switchfly.com>
2019-10-08 17:15:35 -0700

    Cositas

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,11 +1,16 @@
 package tests;
 
+import java.io.File;
 import java.io.IOException;
 import java.security.SecureRandom;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pageobjects.PageFactory;
@@ -27,10 +32,11 @@ public class BaseTest {
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() throws IOException {
-//        String actualTest = this.getClass().getName();
-//        File scrFile = ((TakesScreenshot)driver).getScreenshotAs( OutputType.FILE);
-//        FileUtils.copyFile(scrFile, new File("target/screenshots/"+actualTest+".png"));
+    public void tearDown(ITestResult testResult) throws IOException {
+        if(testResult.getStatus() == ITestResult.FAILURE){
+        String actualTest = this.getClass().getName() + "." + testResult.getMethod();
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs( OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("target/screenshots/"+actualTest+".png"));}
         driver.quit();
     }
 
